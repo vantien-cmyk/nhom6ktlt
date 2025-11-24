e0ad7ab0eab70b28e42236195f02c899bbc24c2d
vinhnq5648-dotcom <vinhnq5648@ut.edu.vn>
2025-11-24 23:26:37 +0700

    Add files via upload

---
+++ bài 4 chương 2.py
@@ -0,0 +1,7 @@
+width = 17
+height = 12.0
+
+print(width // 2, type(width // 2))
+print(width / 2.0, type(width / 2.0))
+print(height / 3, type(height / 3))
+print(1 + 2 * 5, type(1 + 2 * 5))
---
+++ bài 5 chương 2.py
@@ -0,0 +1,3 @@
+c = float(input("Nhập nhiệt độ (°C): "))
+f = c * 9/5 + 32
+print("Nhiệt độ tương ứng (°F):", f)
